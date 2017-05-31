a6f102f66abfe76714c2738d38c725b13bb7745e
Ali N.M. Rumane <alirumane@yahoo.com>
2017-05-31 14:44:12 +0530

    Update style.js

--- assets/themes/bootstrap/js/style.js
+++ assets/themes/bootstrap/js/style.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
      $(window).scroll(function () {
-            if ($(this).scrollTop() > 50) {
+            if ($(this).scrollTop() > 200) {
                 $('#back-to-top').fadeIn();
             } else {
                 $('#back-to-top').fadeOut();
