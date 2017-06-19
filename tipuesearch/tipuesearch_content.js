e99627b114ff0c182da1f01f7f5f38f1ceccda88
Ali N.M. Rumane <alirumane@yahoo.com>
2017-06-19 16:47:01 +0530

    Update tipuesearch_content.js

--- tipuesearch/tipuesearch_content.js
+++ tipuesearch/tipuesearch_content.js
@@ -1,1 +1,182 @@
-var tipuesearch = {"pages": 'url': 'https://alirumane.github.io/search.json'};
+var tipuesearch = {"pages": 
+                  [
+  
+    {
+      "title"    : "Installing MJPEG Streamer on Raspberry Pi",
+      "category" : "Raspberry Pi Tutorials",
+      "tags"     : "Raspberry Pi, MJPEG Streamer, Video",
+      "url"      : "/raspberry%20pi%20tutorials/2016/07/11/mjpg-streamer-on-raspberry-pi",
+      "date"     : "2016-07-11 00:00:00 +0000"
+    } ,
+  
+    {
+      "title"    : "Installing LIGHTTPD web server on Raspberry Pi",
+      "category" : "Raspberry Pi Tutorials",
+      "tags"     : "Raspberry Pi, lighttpd, server",
+      "url"      : "/raspberry%20pi%20tutorials/2016/06/10/installing-lighttpd-on-raspberry-pi",
+      "date"     : "2016-06-10 00:00:00 +0000"
+    } ,
+  
+    {
+      "title"    : "Installing NOOBS OS on Raspberry Pi",
+      "category" : "Raspberry Pi Tutorials",
+      "tags"     : "Raspberry Pi, NOOBS",
+      "url"      : "/raspberry%20pi%20tutorials/2016/04/10/installing-noobs-os-on-raspberry-pi",
+      "date"     : "2016-04-10 00:00:00 +0000"
+    } ,
+  
+    {
+      "title"    : "Turn the wheel, write blog",
+      "category" : "",
+      "tags"     : "blog",
+      "url"      : "/2015/04/10/start-blog",
+      "date"     : "2015-04-10 00:00:00 +0000"
+    } 
+  
+  
+    ,{
+      "title"    : "About",
+      "category" : "",
+      "tags"     : "",
+      "url"      : "/about",
+      "date"     : ""
+    }
+  
+    ,{
+      "title"    : "Archive",
+      "category" : "",
+      "tags"     : "",
+      "url"      : "/archive",
+      "date"     : ""
+    }
+  
+    ,{
+      "title"    : "Atom Feed",
+      "category" : "",
+      "tags"     : "",
+      "url"      : "/atom.xml",
+      "date"     : ""
+    }
+  
+    ,{
+      "title"    : "Blog",
+      "category" : "",
+      "tags"     : "",
+      "url"      : "/blog",
+      "date"     : ""
+    }
+  
+    ,{
+      "title"    : "Categories",
+      "category" : "",
+      "tags"     : "",
+      "url"      : "/categories",
+      "date"     : ""
+    }
+  
+    ,{
+      "title"    : "Contact",
+      "category" : "",
+      "tags"     : "",
+      "url"      : "/contact",
+      "date"     : ""
+    }
+  
+    ,{
+      "title"    : "",
+      "category" : "",
+      "tags"     : "",
+      "url"      : "/feed.xml",
+      "date"     : ""
+    }
+  
+    ,{
+      "title"    : "Home",
+      "category" : "",
+      "tags"     : "",
+      "url"      : "/",
+      "date"     : ""
+    }
+  
+    ,{
+      "title"    : "Pages",
+      "category" : "",
+      "tags"     : "",
+      "url"      : "/pages",
+      "date"     : ""
+    }
+  
+    ,{
+      "title"    : "Projects",
+      "category" : "",
+      "tags"     : "",
+      "url"      : "/projects",
+      "date"     : ""
+    }
+  
+    ,{
+      "title"    : "Resume",
+      "category" : "",
+      "tags"     : "",
+      "url"      : "/resume",
+      "date"     : ""
+    }
+  
+    ,{
+      "title"    : "RSS Feed",
+      "category" : "",
+      "tags"     : "",
+      "url"      : "/rss.xml",
+      "date"     : ""
+    }
+  
+    ,{
+      "title"    : "",
+      "category" : "",
+      "tags"     : "",
+      "url"      : "/search.json",
+      "date"     : ""
+    }
+  
+    ,{
+      "title"    : "Search",
+      "category" : "",
+      "tags"     : "",
+      "url"      : "/search",
+      "date"     : ""
+    }
+  
+    ,{
+      "title"    : "",
+      "category" : "",
+      "tags"     : "",
+      "url"      : "/sitemap.xml",
+      "date"     : ""
+    }
+  
+    ,{
+      "title"    : "Tags",
+      "category" : "",
+      "tags"     : "",
+      "url"      : "/tags",
+      "date"     : ""
+    }
+  
+    ,{
+      "title"    : "",
+      "category" : "",
+      "tags"     : "",
+      "url"      : "/assets/css/style.css",
+      "date"     : ""
+    }
+  
+    ,{
+      "title"    : "HEAD",
+      "category" : "",
+      "tags"     : "",
+      "url"      : "/History",
+      "date"     : ""
+    }
+  
+]
+                  };
